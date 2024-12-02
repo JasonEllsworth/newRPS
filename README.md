4ad82f1fe004b673239bc077b53d5c16b1cb4f91
Jason Ellsworth <jasonells48@icloud.com>
2024-12-02 09:29:27 +0800

    Finished the 1.0 version. It plays one game. Next I will have to add some sort of game loop

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # newRPS
-itsnew
+version 1.0
+    --  minimal UI, just an alert function that accepts some user input.
+    --  input should take the user input and store it into a local veriable.
+    --  This variable should then be compared against a randomly generated comptuer choice.
+    --  Both results should be displayed for the user.
+    --  The corrosponding points box should be updated.
+    --  The program should then ask the user if they plan to play again.
+
+
+Possbile problems :
+    User enters an interger.
+    User enters nothing.
+    User enters Nothing into the feild.
+
+possible combos
+
+Possible tie
+r - r
+p - p
+s - s
+
+s - p win
+s - r
+
+p - r win
+p - s
+
+r - win
+r - p
---
+++ script.js
@@ -0,0 +1,34 @@
+let compNumber = Math.floor(Math.random() * 3);
+let compChoice;
+switch (compNumber) {
+    case 1:
+        compChoice = "paper";
+        break;
+    case 2:
+        compChoice = "scissors";
+        break;
+    case 3:
+        compChoice = "paper";
+        break;
+}
+
+const choiceArray = ["paper", "scissors", "rock"];
+let userInput = prompt("rock paper scissors shoot!")
+let userChoice = userInput.toLowerCase();
+if (choiceArray.includes(userInput)) {
+    rPS();
+}else{
+    console.log("Sorry, you made an illegal entry");
+}
+
+function rPS() {
+    if (userChoice === compNumber) {
+        console.log("Its a  tie!");
+    } else if (userChoice === "rock" && compChoice === "paper" || userChoice === "scissors" && compChoice === "paper" ||
+        userChoice === "paper" && compChoice === "rock") {
+        console.log("You win!");
+    } else {
+        console.log("You lose!");
+    }
+}
+
